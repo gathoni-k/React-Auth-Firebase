83f8fd56a7c1b860a14304fc9b620e358d2c0a6c
gathoni-k <maryg.kahiga@gmail.com>
2022-08-15 12:43:54 +0300

    Login with GitHub

--- src/components/Auth/Button.js
+++ src/components/Auth/Button.js
@@ -2,6 +2,7 @@
 //   FaFacebookSquare, FaGithub, FaGoogle, FaTwitter,
 // } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import { signInWithGithub } from '../../utils/firebase';
 import styles from './signup.module.css';
 
 const icons = {
@@ -12,8 +13,18 @@ const icons = {
 };
 
 function Button({ icon }) {
+  const loginWithGitHub = async () => {
+    const response = await signInWithGithub();
+    console.log(response);
+  };
+  const handleClick = (e) => {
+    const btnName = e.target.parentNode.name;
+    if (btnName === 'github') {
+      loginWithGitHub();
+    }
+  };
   return (
-    <button type="button" className={styles.socialiconwrapper} arial-label={`Login with ${icon}`}>
+    <button type="button" name={icon} className={styles.socialiconwrapper} arial-label={`Login with ${icon}`} onClick={handleClick}>
       <img src={icons[icon]} alt={icon} />
     </button>
   );
