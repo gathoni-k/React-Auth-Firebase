625fd99ba934efb90e9e675d0789f3b17aed37ef
gathoni-k <maryg.kahiga@gmail.com>
2022-08-15 12:51:40 +0300

    Display username if logged in

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,16 @@
+import { useContext, useEffect, useState } from 'react';
+import AuthContext from '../../context/Auth/AuthContext';
+import getNameFromEmail from '../../helpers/getNameFromEmail';
+
 export default function Home() {
+  const value = useContext(AuthContext);
+  const [name, setname] = useState('');
+  useEffect(() => {
+    if (value.user.email) {
+      setname(getNameFromEmail(value.user.email));
+    }
+  }, [value]);
   return (
-    <div>Home</div>
+    <div>{name || 'Login'}</div>
   );
 }
